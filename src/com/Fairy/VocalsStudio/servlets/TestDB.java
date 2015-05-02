8ab61b3d9bc01f05ad10ed8b2aa574e59168ffb0
fairy <fairy.freyja@gmail.com>
2015-05-03 02:52:39 +0300

    first commit
    Set connection to TomCat and MySql DB

---
+++ src/com/Fairy/VocalsStudio/servlets/TestDB.java
@@ -0,0 +1,39 @@
+package com.Fairy.VocalsStudio.servlets;
+
+import com.Fairy.VocalsStudio.models.DBConnectionFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Wyvern on 03.05.2015.
+ */
+public class TestDB extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection con = null;
+        try {
+            con = DBConnectionFactory.getDBConnection();
+            ResultSet rs = con.createStatement().executeQuery("SELECT * FROM users");
+            while (rs.next()) {
+                resp.getWriter().println(rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
